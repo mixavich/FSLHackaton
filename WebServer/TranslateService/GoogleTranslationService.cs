2f218232a775403646e4351fc693b31454fef29c
Michael N <michael-42@mail.ru>
2017-11-18 19:43:25 +0300

    added translate to web prj

---
+++ WebServer/TranslateService/GoogleTranslationService.cs
@@ -0,0 +1,26 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranslateService
+{
+    public static class GoogleTranslationService
+    {
+        public static string GetTranslation(string OriginalWord, string TranslationLanguage)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            TranslationClient client = TranslationClient.Create();
+
+            var TranslatedWord = client.TranslateText(OriginalWord, TranslationLanguage);
+
+            return TranslatedWord.TranslatedText;
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ WebServer/WebServer/Controllers/TranslateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Translate")]
+    public class TranslateController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{word}")]
+        public string Get(string word)
+        {
+            return TranslateService.GoogleTranslationService.GetTranslation(word, "ru");
+        }
+    }
+}
