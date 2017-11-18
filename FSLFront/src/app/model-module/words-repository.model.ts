9a18bc369c71c992e57c5085c3324d5bcb874dc8
Nikita Korobkov <nikkorobk@gmail.com>
2017-11-18 17:00:37 +0300

    #2 Create Dic
    - Bootstrap added

--- FSLFront/src/app/model-module/words-repository.model.ts
+++ FSLFront/src/app/model-module/words-repository.model.ts
@@ -3,7 +3,11 @@ import {Word} from './word.model';
 
 @Injectable()
 export class WordsRepository {
-  private words: Word[] = [new Word(12,'Оригинал','Original')];
+  private words: Word[] = [new Word(1, 'Оригинал', 'Original'),
+    new Word(2, 'Оригинал', 'Original'),
+    new Word(3, 'Оригинал', 'Original'),
+    new Word(4, 'Оригинал', 'Original'),
+    new Word(5, 'Оригинал', 'Original')];
 
   getWords(): Word[] {
     return this.words;
@@ -29,7 +33,7 @@ export class WordsRepository {
     if (foundWordIndex === -1) {
       console.log('unexpected behaviour found, word not found');
     } else {
-      this.words = this.words.splice(foundWordIndex,1)
+      this.words = this.words.splice(foundWordIndex, 1)
     }
   }
 
