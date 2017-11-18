0af80f71fa229d24dc2febc9d5818161a05c0524
tsepes.vlad <tsepes.vlad@hotmail.com>
2017-11-18 18:42:04 +0300

    services

---
+++ Services/GooglePronunciationService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace GooglePronunciationService
+{
+    public static class GooglePronunciationService
+    {
+        public static string UrlPartOne = "https://translate.google.com.vn/translate_tts?ie=UTF-8&q=";
+        public static string UrlPartTwo = "&client=tw-ob";
+
+        public static string GetPronunciation(string PronunciationWord, string PronunciationLanguage)
+        {
+            WebClient PronunciationWebClient = new WebClient();
+
+            return null;
+        }
+    }
+}
